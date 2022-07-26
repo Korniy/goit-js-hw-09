6234b70db6147689bb728d5f23ebb4a15104c50f
Volodymyr Korniychuk <korniychuk49@gmail.com>
2022-07-26 18:49:34 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,78 @@
 
+
+// const buttonStart = document.querySelector('[data-start]');
+// const buttonStop = document.querySelector('[data-stop]');
+// let changesColor;
+
+// buttonStart.addEventListener('click', pressTheStartButton);
+// function pressTheStartButton() {
+    
+//     changesColor = setInterval(() => {
+//         document.body.style.backgroundColor = getRandomHexColor();
+//     }, 1000);
+//     abc.start();
+// };
+
+
+// buttonStop.addEventListener('click', pressTheStopButton)
+// function pressTheStopButton() {
+//     clearInterval(changesColor);
+//     abc.stop();
+// };
+
+// const abc = {
+//     isActive: false,
+//     start() {
+//         if (this.isActive) {
+//             return;
+//         }
+//         this.isActive = true;
+//          changesColor = setInterval(() => {
+//         document.body.style.backgroundColor = getRandomHexColor();
+//     }, 1000);
+//     },
+
+//     stop() {
+//         this.isActive = false;
+//     }
+// }
+
+// Функція генерування випадкового кольору
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
+let changesColor;
+
+buttonStart.addEventListener('click', pressTheStartButton);
+function pressTheStartButton() {
+     startIsDisabled.start();
+};
+
+buttonStop.addEventListener('click', pressTheStopButton)
+function pressTheStopButton() {
+    startIsDisabled.stop();
+};
+
+const startIsDisabled = {
+    isActive: false,
+
+    start() {
+        if (this.isActive) {
+            return;
+        };
+
+        this.isActive = true;
+        changesColor = setInterval(() => {
+            document.body.style.backgroundColor = getRandomHexColor();
+        }, 1000);
+    },
+
+    stop() {
+        clearInterval(changesColor);
+        this.isActive = false;
+    }
+};
